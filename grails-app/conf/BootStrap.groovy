f839615632046d1ae10d0f33e19e1712c76fa45f
Yannis De Wolf <yannisdewolf@gmail.com>
2014-03-01 16:37:51 +0100

    json response

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,20 @@
+import grails.converters.JSON
+import gtastock.StockEntry
+
 class BootStrap {
 
     def init = { servletContext ->
+	    JSON.registerObjectMarshaller(StockEntry) {
+			def output = [:]
+			output['share'] = it.share
+			output['timestamp'] = it.timestamp
+			output['value'] = it.value
+			
+			return output
+	    }
     }
     def destroy = {
     }
+	
+	
 }
--- grails-app/controllers/gtastock/StockEntryController.groovy
+++ grails-app/controllers/gtastock/StockEntryController.groovy
@@ -1,5 +1,7 @@
 package gtastock
 
+import grails.converters.JSON
+
 class StockEntryController {
 
 	def StockEntryService stockEntryService
@@ -8,7 +10,7 @@ class StockEntryController {
 		
 		def entries = stockEntryService.getStockData("WIZ")
 		
-		[entryList:entries]
+		render entries as JSON
 		
 	}
 }
